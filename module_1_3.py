b30147fadaf7d9c96bf60cbd680d8aa9f724b9f9
Chost24 <markisov24@mail.ru>
2024-08-23 18:34:43 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,5 @@
+homework = 12
+hours = 1.5
+name = "Python"
+task_time = (hours / homework)
+print('Курс:',name,',всего задач:',homework,',затрачено часов:',hours,',среднее время выполнения:',task_time)
